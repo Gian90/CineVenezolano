8ae8856acd03e79af9110ee7870c04aedb2f2390
darkjuan193 <darkjuan193@gmail.com>
2016-05-04 16:24:12 -0430

    Cambios a header_usu.php
    Ni idea maricoooooo

---
+++ header_usu.php
@@ -0,0 +1,212 @@
+<!--   <header>
+<!--    <div class="container " height="300">
+        <div class="row fila_2">
+            <a href="index.html"> <img src="Foto0042.jpg" class="img-responsive"></a>
+        </div>
+    </div>  -->
+    <!--  .............................................................................................. -->
+
+      <!-- 		 <div class="container">
+            <div class="row">
+
+        <div  class="col-md-2 centered">
+
+                  <img src="logopruebaCIC.png" class="img-responsive" >
+                    </div>
+
+                    <div   class="col-md-8 centered"  >
+
+                    <img src="Script.png" style="height:150" class="img-responsive" >
+                    </div>
+
+                    <div  class="col-md-2 centered">
+
+                      <div class="row">
+                        <div class="col-md-2" >
+
+
+          <a class="btn btn-block btn-social ">
+                <i class="fa fa-twitter "></i>
+            </a>
+
+                      </div>
+                        <div class="col-md-2" >
+                          <a class="btn btn-block btn-social ">
+                <i class="fa fa-facebook"></i>
+            </a>
+                        </div>
+                        <div class="col-md-2" >
+                          <a class="btn btn-block btn-social ">
+                <i class="fa  fa-instagram"></i>
+            </a>
+                      </div>
+                    </div>
+          </div>
+
+
+          </div>
+              </div>
+
+
+
+    <!--  .............................................................................................. -->
+
+
+
+<!--       <div class="container">
+        <div class="row">
+            <nav role="navigation" class="navbar navbar-default visible-sm visible-md  visible-lg ">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex2-collapse">
+                        <span class="sr-only">Cambiar Navegacion</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class=" collapse navbar-collapse  btn-group btn-group-justified   navbar-ex2-collapse">
+                    <ul class="btn nav nav-justified nav-pills navbar-btn">
+                        <li role="presentation" >
+                            <a class="bg-info" href="#">Home</a>
+                        </li>
+                        <li role="presentation" class="active">
+                            <a href="#">Pelicula de la A-Z</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#">Pelicula por Año</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#">Pelicula por genero y tematica</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#">Empresas Filiales</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#">Noticias</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header> -->
+
+
+
+
+<!--    <div class="container " height="300">
+        <div class="row fila_2">
+            <a href="index.html"> <img src="Foto0042.jpg" class="img-responsive"></a>
+        </div>
+    </div>  -->
+    <!--  .............................................................................................. -->
+
+<!--
+<div  class="col-md-2 centered">
+
+<img src="logopruebaCIC.png" class="img-responsive" >
+</div>
+-->
+
+<!--  <div  class="col-md-2 centered">
+    <div class="row">
+      <div class="col-md-2" >
+        <a class="btn btn-block btn-social ">
+          <i class="fa fa-twitter "></i>
+        </a>
+      </div>
+      <div class="col-md-2" >
+        <a class="btn btn-block btn-social ">
+          <i class="fa fa-facebook"></i>
+        </a>
+      </div>
+      <div class="col-md-2" >
+        <a class="btn btn-block btn-social ">
+          <i class="fa  fa-instagram"></i>
+        </a>
+      </div>
+    </div>
+  </div>-->
+
+
+
+
+
+    <!--  .............................................................................................. -->
+    <header>
+
+      <!--Imagen Principal--
+      <div class="container">
+        <div class="row">
+          <img src="imagenes\Pag\SCRIPT(2159x557).png" style="height:150;" class="img-responsive img-rounded">
+        </div>
+      </div>
+      <!--Fin De Imagen Principal-->
+
+      <!--Inicio Del Menu De Opciones-->
+      <div class="container">
+        <div class="row">
+          <img src="imagenes\Pag\SCRIPT(2159x557).png" style="height:150;" class="img-responsive img-rounded">
+          <nav class="navbar navbar-inverse menu" role="navigation">
+            <!-- navbar-inverse menu
+
+            El logotipo y el icono que despliega el menú se agrupan
+            para mostrarlos mejor en los dispositivos móviles -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#BarraDeNavegacion">
+                <span class="sr-only">Desplegar/Ocultar Menu Header</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">SCRIPT</a>
+            </div>
+            <!--INICIA EL MENU HEADER  -->
+            <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+            otro elemento que se pueda ocultar al minimizar la barra -->
+            <div class="collapse navbar-collapse" id="BarraDeNavegacion">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Inicio</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                    Opinion <b class="caret"></b>
+                  </a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Peliculas</a></li>
+                    <li><a href="#">Actores</a></li>
+                    <li><a href="#">Trailers</a></li>
+                  <!--  <li class="divider"></li>
+                    <li><a href="#">Acción #4</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Acción #5</a></li>-->
+                  </ul>
+                </li>
+                <li><a href="#">Buscador</a></li>
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Nosotros</a></li>
+              </ul>
+
+              <ul class="nav navbar-nav navbar-right">
+                <button type="submit" class="btn btn-default">
+                  <span class="glyphicon glyphicon-user"> Entra</span>
+                </button>
+                <button type="submit" class="btn btn-default">
+                  <span class="glyphicon glyphicon-th-list"> Registrate</span>
+                </button>
+              </ul>
+
+              <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Buscar">
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  <span class="glyphicon glyphicon-search"></span>
+                </button>
+              </form>
+
+            </div>
+          </nav>
+        </div>
+      </div>
+      <!--Fin Del Menu de Opciones-->
+    </header>
